816c82d1c6c42f49caec6e6731671d7e09705785
himici <jianfeng.ge@1rock.cn>
2017-10-30 16:53:55 +0800

    add

--- src/components/header/title_header.vue
+++ src/components/header/title_header.vue
@@ -40,7 +40,14 @@
           <div class="detial-wrapper clearfix">
               <div class="detail-main">
                   <h1 class="name">{{ seller.name }}</h1>
-                  <star v-bind:size="48" v-bind:score="seller.score"></star>
+                  <div class="star-wrapper">
+                      <star v-bind:size="48" v-bind:score="seller.score"></star>
+                  </div>
+                  <div class="title">
+                      <div class="line"></div>
+                      <div class="text">优惠信息</div>
+                      <div class="line"></div>
+                  </div>
               </div>
           </div>
           <div class="detail-close">
@@ -252,6 +259,27 @@
                       text-align: center;
                       font-size: 16px;
                       font-weight: 700;
+                  }
+                  .star-wrapper{
+                      margin-top: 18px;
+                      padding: 2px 0;
+                      /*父布局detial-wrapper 需要设置宽度，否则不会居中，也是，不知道父布局的宽度，怎么居中*/
+                      text-align: center;
+                  }
+                  .title{
+                      display: flex;
+                      width: 80%;
+                      margin: 30px auto 24px auto;
+                      .line{
+                          flex: 1;
+                          position: relative;
+                          top: -6px;
+                          border-bottom: 1px solid rgba(255,255,255,0.2);
+                      }
+                      .text{
+                          padding: 0 12px;
+                          font-size: 14px;
+                      }
                   }
               }
           }
