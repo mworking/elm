b19e12ecca2642a7cddf4f6a5824c08f10becf46
himici <jianfeng.ge@1rock.cn>
2017-10-31 11:16:22 +0800

    add goods

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,51 @@
 <template>
-  <div>
-    this is goods
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
   </div>
 </template>
 
-<script>
-  export default{}
+<script type="text/ecmascript-6">
+
+  const ERR_OK = 0;
+
+  export default{
+    data() {
+      return {
+        goods:[]
+      }
+    },
+    props:{
+      seller:{
+        type: Object
+      }
+    },
+    created(){
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body;
+        if(response.errno === ERR_OK){
+          this.goods = response.data;
+        }
+      });
+    }
+  }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+   .goods{
+     display: flex;
+     position: absolute;
+     top:174px;
+     bottom: 46px;
+     width: 100%;
+     overflow: hidden;
+     .menu-wrapper{
+       flex: 0 0 80px;
+       width: 80px;
+       background: #f3f5f7;
+     }
+     .foods-wrapper{
+       flex: 1;
+     }
+   }
 </style>
